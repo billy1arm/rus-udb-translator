08f32c10c78ef7627fc0fbc93f6f8249bb06e8a7
oleynik.a.v@gmail.com <oleynik.a.v@gmail.com@46511d94-52d4-11de-b413-c5e99dc0a8e2>
2009-06-06 20:09:45 +0000

    Наполнение корневой папки

---
+++ trunk/www/index.php
@@ -0,0 +1,20 @@
+﻿<?php
+define ('IN_MANGOS_RUS', FALSE);
+
+include_once ('config.php');
+
+include_once ($config['inc_dir'] . 'db.php');
+include_once ($config['inc_dir'] . 'common.php');
+
+if (!isset($_REQUEST['action']) || empty($_REQUEST['action']) || $work->url_check())
+{
+	$action = 'main';
+}
+else
+{
+	$action = $_REQUEST['action'];
+	if(!@fopen($config['site_dir'] . 'action/' . $action . '.php', 'r')) $action = 'main';
+}
+
+include_once($config['site_dir'] . 'action/' . $action . '.php');
+?>
