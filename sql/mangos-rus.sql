a46efebff254182ffec780f6c0495fd1a2e009d2
oleynik.a.v@gmail.com <oleynik.a.v@gmail.com@46511d94-52d4-11de-b413-c5e99dc0a8e2>
2009-06-12 12:56:35 +0000

    В предыдущей ревизии был залит не полный дамп базы

--- sql/mangos-rus.sql
+++ sql/mangos-rus.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Июн 12 2009 г., 15:52
+-- Время создания: Июн 12 2009 г., 15:55
 -- Версия сервера: 5.1.34
 -- Версия PHP: 5.2.9-2
 
@@ -18,6 +18,29 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- База данных: `mangos-rus`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `config`
+--
+
+DROP TABLE IF EXISTS `config`;
+CREATE TABLE IF NOT EXISTS `config` (
+  `name` varchar(50) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `config`
+--
+
+INSERT INTO `config` (`name`, `value`) VALUES
+('rev_orig', '21'),
+('rev_rus', '22'),
+('theme', 'default'),
+('title', 'Mangos-Rus');
 
 -- --------------------------------------------------------
 
@@ -60,6 +83,71 @@ INSERT INTO `config_db` (`id`, `last_recalculate`, `db`, `name_rus`, `name_orig`
 (12, '2009-06-12 15:42:00', 'mangos', 'mangos_string', 'mangos_string', 260, 260, 649, NULL, NULL, 'Cодержит все сообщения, посылаемые сервером игрокам. Основная цель таблицы - перевод этих сообщений.'),
 (13, '2009-06-12 15:42:28', 'scriptdev2', 'custom_texts', 'custom_texts', 0, 0, 0, NULL, NULL, 'Таблица используеться для предоставления переведенных произвольных текстов в SD2 клиентам с определенной локализацией.'),
 (14, '2009-06-12 15:42:31', 'scriptdev2', 'script_texts', 'script_texts', 1, 1, 1559, NULL, NULL, 'Таблица используеться для предоставления переведенных сообщений в SD2 клиентам с определенной локализацией.');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `config_table`
+--
+
+DROP TABLE IF EXISTS `config_table`;
+CREATE TABLE IF NOT EXISTS `config_table` (
+  `id_table` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `custom` tinyint(1) NOT NULL DEFAULT '0',
+  `default` tinyint(1) NOT NULL DEFAULT '0',
+  `row_orig_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_table`,`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `config_table`
+--
+
+INSERT INTO `config_table` (`id_table`, `name`, `custom`, `default`, `row_orig_name`) VALUES
+(1, 'content', 0, 1, 'content_default'),
+(2, 'subject', 0, 0, 'subject'),
+(2, 'text', 0, 0, 'text'),
+(3, 'name', 0, 0, 'name'),
+(3, 'subname', 0, 0, 'subname'),
+(4, 'name', 0, 0, 'name'),
+(4, 'castbarcaption', 0, 0, 'castBarCaption'),
+(5, 'name', 0, 0, 'name'),
+(5, 'description', 0, 0, 'description'),
+(6, 'option_text', 0, 0, 'option_text'),
+(6, 'box_text', 0, 0, 'box_text'),
+(7, 'Text0_0', 0, 0, 'text0_0'),
+(7, 'Text0_1', 0, 0, 'text0_1'),
+(7, 'Text1_0', 0, 0, 'text1_0'),
+(7, 'Text1_1', 0, 0, 'text1_1'),
+(7, 'Text2_0', 0, 0, 'text2_0'),
+(7, 'Text2_1', 0, 0, 'text2_1'),
+(7, 'Text3_0', 0, 0, 'text3_0'),
+(7, 'Text3_1', 0, 0, 'text3_1'),
+(7, 'Text4_0', 0, 0, 'text4_0'),
+(7, 'Text4_1', 0, 0, 'text4_1'),
+(7, 'Text5_0', 0, 0, 'text5_0'),
+(7, 'Text5_1', 0, 0, 'text5_1'),
+(7, 'Text6_0', 0, 0, 'text6_0'),
+(7, 'Text6_1', 0, 0, 'text6_1'),
+(7, 'Text7_0', 0, 0, 'text7_0'),
+(7, 'Text7_1', 0, 0, 'text7_1'),
+(8, 'Text', 0, 0, 'text'),
+(9, 'icon_name', 0, 0, 'icon_name'),
+(10, 'Title', 0, 0, 'Title'),
+(10, 'Details', 0, 0, 'Details'),
+(10, 'Objectives', 0, 0, 'Objectives'),
+(10, 'OfferRewardText', 0, 0, 'OfferRewardText'),
+(10, 'RequestItemsText', 0, 0, 'RequestItemsText'),
+(10, 'EndText', 0, 0, 'EndText'),
+(10, 'ObjectiveText1', 0, 0, 'ObjectiveText1'),
+(10, 'ObjectiveText2', 0, 0, 'ObjectiveText2'),
+(10, 'ObjectiveText3', 0, 0, 'ObjectiveText3'),
+(10, 'ObjectiveText4', 0, 0, 'ObjectiveText4'),
+(11, 'content', 0, 1, 'content_default'),
+(12, 'content', 0, 1, 'content_default'),
+(13, 'content', 0, 1, 'content_default'),
+(14, 'content', 0, 1, 'content_default');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
